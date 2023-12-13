d1e273e2f8b26c422715d05bec6d56928b948821
AsitaEmmanuella <asitaemmanuella19@gmail.com>
2023-12-13 05:12:04 -0800

    updated the list_execute.c file

--- list_execute.c
+++ list_execute.c
@@ -15,103 +15,18 @@
  */
 void parseArguments(const char *command, char *args[])
 {
-	char *token = strtok((char *)command, " ");
-
+	char *copy = strdup(command);
+	char *token = strtok(copy, " ");
 	int i = 0;
 
 	while (token != NULL && i < MAX_COMMAND_LENGTH - 1)
 	{
-		if (i > 0)
-		{
-			fprintf(stderr, "Error: Only single-word commands are allowed.\n");
-			exit(EXIT_FAILURE);
-		}
 		args[i++] = token;
 		token = strtok(NULL, " ");
 	}
 	args[i] = NULL;
-}
 
-/**
- * printEnvironment - prints all environment variables to the console
- */
-
-void printEnvironment(void)
-{
-	char **env = environ;
-
-	while (*env != NULL)
-	{
-		printf("%s\n", *env);
-		env++;
-	}
-}
-
-/**
- * executeChild - takes a command string and executes it as a child process
- * @command: pointer to the command string
- */
-
-void executeChild(const char *command)
-{
-	char *args[MAX_COMMAND_LENGTH];
-	char *path;
-	char executable[MAX_COMMAND_LENGTH];
-
-	parseArguments(command, args);
-
-	if (strcmp(args[0], "env") == 0)
-	{
-		printEnvironment();
-		exit(EXIT_SUCCESS);
-	}
-
-	path = getenv("PATH");
-	if (path == NULL)
-	{
-		fprintf(stderr, "Error: PATH environment variable not set.\n");
-		exit(EXIT_FAILURE);
-	}
-
-	snprintf(executable, sizeof(executable), "%s/%s", path, args[0]);
-
-	if (access(executable, X_OK) == 0)
-	{
-		if (execv(executable, args) == -1)
-		{
-			perror("execv");
-			fprintf(stderr, "Error: Execution failed for command: %s\n", args[0]);
-			exit(EXIT_FAILURE);
-		}
-	}
-
-	fprintf(stderr, "Error: Command not found: %s\n", args[0]);
-	exit(EXIT_FAILURE);
-}
-
-/**
- * executeParent - waits for a child process execution and report its status
- * @pid:  Process ID of the child process to wait for
- */
-
-void executeParent(pid_t pid)
-{
-	int status;
-
-	if (waitpid(pid, &status, 0) == -1)
-	{
-		perror("waitpid");
-		exit(EXIT_FAILURE);
-	}
-
-	if (WIFEXITED(status))
-	{
-		printf("Successfully Executed.\n");
-	}
-	else
-	{
-		printf("Command Failed.\n");
-	}
+	free(copy);
 }
 
 /**
@@ -130,12 +45,34 @@ void executeCommand(const char *command)
 	}
 	else if (pid == 0)
 	{
-		/* Child process */
-		executeChild(command);
+		char *args[MAX_COMMAND_LENGTH];
+
+		parseArguments(command, args);
+
+		if (execve(args[0], args, NULL) == -1)
+		{
+			perror("execve");
+			_exit(EXIT_FAILURE);
+		}
 	}
 	else
 	{
-		/* Parent process */
-		executeParent(pid);
+		int status;
+
+		if (waitpid(pid, &status, 0) == -1)
+		{
+			perror("waitpid");
+			exit(EXIT_FAILURE);
+		}
+
+		if (WIFEXITED(status))
+		{
+			write(1, "Successfully Executed.\n", 24);
+		}
+		else
+		{
+			write(1, "Command Failed.\n", 16);
+		}
 	}
 }
+
