1a3dfa92ddc1ffb8ccbd50255c3d383386291f39
AsitaEmmanuella <asitaemmanuella19@gmail.com>
2023-12-13 17:29:54 -0800

    created five new files

---
+++ sigint_handler.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+
+/**
+ * get_sigint - function to handle the crtl + c call in prompt
+ * @sig: Signal handler
+ *
+ * Return: void
+ */
+
+void get_sigint(int sig)
+{
+	(void)sig;
+	write(STDOUT_FILENO, "\n^-^ ", 5);
+}
+
